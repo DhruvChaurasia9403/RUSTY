48684c0b800d39f259339fceb7cfbb4400534f15
Dhruv Chaurasia <www.thedc2@gmail.com>
2025-08-19 03:07:19 +0530

    Rust a Low level Language, but high level too

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,5 +1,20 @@
 fn main() {
     println!("Hello, world!");
     let num:u8 = 5;
+
     println!("the num stores an integer value equals : {}", num);
+    println!();
+
+    // Demonstrating string usage &str 
+    let s = "Rust is a Low-level programming language";
+    println!("String s is: {}",s);
+    println!();
+
+    
+    // Demonstrating mutable string
+    let mut s1 = String::from("Rust is a Low-level programming language");
+    s1.push_str(" and it is also a High-level programming language");
+    println!("Updated string s1 is: {}", s1);
+
+
 }
