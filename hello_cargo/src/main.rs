31394b903fb34475eba2d2bfc2f67c1cb5038991
Dhruv Chaurasia <www.thedc2@gmail.com>
2025-08-23 02:45:13 +0530

    Ownership , Ownership dealing , Ownership Avoidance

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -2,6 +2,8 @@ mod string_demo;
 mod tupple_demo;
 mod functions_demo;
 mod ownership_inrust;
+mod ownership_understanding;
+mod ownership_dealing;
 
 fn main() {
     println!("Hello, world!");
@@ -56,4 +58,26 @@ fn main() {
     println!("----------------END----------------");
     println!();
     println!();
+
+
+
+    println!("Ownership in Rust Rule 2 : there can only be one owner at a time");
+    println!("--------------START----------------");
+    ownership_understanding::owner2();
+    println!("if you will try to print s2 it will throw error because once the s2 was the owner of the \"world\"\n,but now its not and rust has thrown it in garbage!!");
+    println!("----------------END----------------");
+    println!();
+    println!(); 
+
+
+
+    println!("Ownership Avoidance");
+    println!("--------------START----------------");
+    println!("we need to avoid ownership sometimes \nwhy?\nsee the file and find out\nhow?\nit has 3 methods, see the file ownership_dealing");
+    println!("Method 1 : making tupple\nMethod 2 : cloning\nMethod 3 : Borrowing - (Next File)");
+    ownership_dealing::owner3();
+    println!("----------------END----------------");
+    println!();
+    println!();
 }
+
